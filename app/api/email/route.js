284635772b648215c2b4fc6a05ff3d33551c397a
Hamza Waqas <hamza@creationnext.com>
2025-03-28 21:45:08 +0500

    Update route.js

--- app/api/email/route.js
+++ app/api/email/route.js
@@ -4,6 +4,7 @@ import nodemailer from "nodemailer";
 export async function POST(req) {
   try {
     const body = await req.json();
+    console.log(body, "get data from react native app");
 
     if (!body.name) {
       return NextResponse.json(
@@ -42,7 +43,7 @@ export async function POST(req) {
   } catch (error) {
     console.error("Server Error:", error);
     return NextResponse.json(
-      { error: "Failed to send email", details: error.message },
+      { error: "Failed to send email hamza", details: error.message },
       { status: 500 }
     );
   }
