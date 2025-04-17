7946606d10dddad18e71e8f36c03391d0b1bac62
Hamza Waqas <hamza@creationnext.com>
2025-04-17 20:58:38 +0500

    Update route.js

--- app/api/rohani_ilaj_form/route.js
+++ app/api/rohani_ilaj_form/route.js
@@ -3,7 +3,8 @@ import nodemailer from "nodemailer";
 export async function POST(req) {
   try {
     const body = await req.json();
-    if (!body.patientName || !body.email) {
+    console.log(body, "Received data from React Native app");
+    if (!body.name || !body.email) {
       return NextResponse.json(
         { error: "Name and Email are required!" },
         { status: 400 }
@@ -17,7 +18,7 @@ export async function POST(req) {
       },
     });
     const adminMailOptions = {
-      from: `"New Request from ${body.patientName}" <${body.email}>`,
+      from: `"New Request from ${body.name}" <${body.email}>`,
       to: "hamzawaqas194@gmail.com",
       subject: "New Form Submission Received",
       html: `
@@ -27,9 +28,16 @@ export async function POST(req) {
         <p><strong>Mother Name:</strong> ${body.motherName || "N/A"}</p>
         <p><strong>Country:</strong> ${body.country || "N/A"}</p>
         <p><strong>City:</strong> ${body.cityName || "N/A"}</p>
+           <p><strong>Date of Birth:</strong> ${body.day}-${body.month}-${
+        body.year
+      }</p>
+        <p><strong>Age:</strong> ${body.age || "N/A"}</p>
+       
+        <p><strong>Gender:</strong> ${body.gender || "N/A"}</p>
+        <p><strong>Status:</strong> ${body.status || "N/A"}</p>
         <p><strong>WhatsApp Number:</strong> ${body.whatsappNumber || "N/A"}</p>
         <p><strong>Email:</strong> ${body.email}</p>
-        <p><strong>Nature of Desease:</strong> ${body.desease || "N/A"}</p>
+        <p><strong>Nature of disease:</strong> ${body.disease || "N/A"}</p>
         <hr/>
         <p>Reply directly to this email to respond to the user.</p>
       `,
@@ -39,7 +47,7 @@ export async function POST(req) {
       to: body.email,
       subject: "Your Form Submission Received",
       html: `
-        <p>Hello <strong>${body.patientName}</strong>,</p>
+        <p>Hello <strong>${body.name}</strong>,</p>
         <p>We have received your request and will get back to you soon.</p>
         <p>Regards,<br/>Support Team</p>
       `,
