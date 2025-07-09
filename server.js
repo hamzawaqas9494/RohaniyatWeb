2530b14273986222954b9ce0bc3076f23dfdc1be
Hamza Waqas <151855666+hamzawaqas9494@users.noreply.github.com>
2025-07-09 20:18:22 +0500

    s

--- server.js
+++ server.js
@@ -1,23 +1,48 @@
-// server.js
-require('dotenv').config();
+// // server.js
+// require('dotenv').config();
 
-console.log("📦 ENV Loaded:");
+// console.log("📦 ENV Loaded:");
 
-const { createServer } = require('http');
-const { parse } = require('url');
-const next = require('next');
+// const { createServer } = require('http');
+// const { parse } = require('url');
+// const next = require('next');
 
-const port = process.env.PORT || 3000;
-const dev = false; // production mode
-const app = next({ dev });
-const handle = app.getRequestHandler();
+// const port = process.env.PORT || 3000;
+// const dev = false; // production mode
+// const app = next({ dev });
+// const handle = app.getRequestHandler();
+
+// app.prepare().then(() => {
+//   createServer((req, res) => {
+//     const parsedUrl = parse(req.url, true);
+//     handle(req, res, parsedUrl);
+//   }).listen(port, (err) => {
+//     if (err) throw err;
+//     console.log(`> Ready on http://localhost:${port}`);
+//   });
+// });
+
+
+const express = require('express')
+const next = require('next')
+const path = require('path')
+require('dotenv').config()
+
+const port = process.env.PORT || 3000
+const dev = false
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
-  }).listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  });
-});
+  const server = express()
+
+  // Serve .next and public
+  server.use('/_next', express.static(path.join(__dirname, '.next')))
+  server.use('/public', express.static(path.join(__dirname, 'public')))
+
+  server.all('*', (req, res) => handle(req, res))
+
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,25 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+// const defaultTheme = require("tailwindcss/defaultTheme");
 
-module.exports = {
-  content: [
-    "./pages/**/*.{js,jsx,tsx,mdx}",
-    "./components/**/*.{js,jsx,tsx,mdx}",
-    "./app/**/*.{js,jsx,tsx,mdx}",
-    "./ui/**/*.{js,jsx,tsx,mdx}",
-    "./icons/**/*.{js,jsx,tsx,mdx}",
-  ],
-  theme: {
-    container: {
-      center: true,
-    },
-    extend: {
-      fontFamily: {
-        // sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
-        urdu: ['UrduFont', 'sans-serif'], 
-      },
-    },
-  },
-  plugins: [],
-};
+// module.exports = {
+//   tailwindcss: {},
+//     autoprefixer: {},
+//   content: [
+//     "./pages/**/*.{js,jsx,tsx,mdx}",
+//     "./components/**/*.{js,jsx,tsx,mdx}",
+//     "./app/**/*.{js,jsx,tsx,mdx}",
+//     "./ui/**/*.{js,jsx,tsx,mdx}",
+//     "./icons/**/*.{js,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     container: {
+//       center: true,
+//     },
+//     extend: {
+//       fontFamily: {
+//         // sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+//         urdu: ['UrduFont', 'sans-serif'], 
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
