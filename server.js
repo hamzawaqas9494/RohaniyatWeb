50a6f487279aa65383c27ffffcf73623c3428a5a
Hamza Waqas <151855666+hamzawaqas9494@users.noreply.github.com>
2025-07-10 12:05:17 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,48 +1,26 @@
-// // server.js
-// require('dotenv').config();
+// server.js
+require('dotenv').config();
 
-// console.log("📦 ENV Loaded:");
+console.log("📦 ENV Loaded:");
 
-// const { createServer } = require('http');
-// const { parse } = require('url');
-// const next = require('next');
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
 
-// const port = process.env.PORT || 3000;
-// const dev = false; // production mode
-// const app = next({ dev });
-// const handle = app.getRequestHandler();
-
-// app.prepare().then(() => {
-//   createServer((req, res) => {
-//     const parsedUrl = parse(req.url, true);
-//     handle(req, res, parsedUrl);
-//   }).listen(port, (err) => {
-//     if (err) throw err;
-//     console.log(`> Ready on http://localhost:${port}`);
-//   });
-// });
-
-
-const express = require('express')
-const next = require('next')
-const path = require('path')
-require('dotenv').config()
-
-const port = process.env.PORT || 3000
-const dev = false
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = process.env.PORT || 3000;
+const dev = false; // production mode
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = express()
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
 
-  // Serve .next and public
-  server.use('/_next', express.static(path.join(__dirname, '.next')))
-  server.use('/public', express.static(path.join(__dirname, 'public')))
 
-  server.all('*', (req, res) => handle(req, res))
 
-  server.listen(port, () => {
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
